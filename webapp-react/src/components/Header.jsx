566373c4986eb6accae9240a9207f24a92c098b3
MattiaToscano <mattia1997ct@gmail.com>
2025-05-30 10:18:07 +0200

    [UPD] Aggiunta componente header

---
+++ webapp-react/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    <nav className="navbar bg-primary mb-4">
+        <div className="container-fluid">
+            <Link className="navbar-brand text-white" to="/">
+                Book Store
+            </Link>
+        </div>
+    </nav>
+}
