3341957b06e62077dab1be17ce35ed085fb416cf
MattiaToscano <mattia1997ct@gmail.com>
2025-05-30 10:36:04 +0200

    [UPD] Updated HomePage component and added BookPage component

--- webapp-react/src/App.jsx
+++ webapp-react/src/App.jsx
@@ -1,13 +1,13 @@
 import DefaultLayout from './layouts/DefaultLayout';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<DefaultLayout />}>
-          <Route index element={<h1>Homepage</h1>}></Route>
+          <Route index element={<HomePage />}></Route>
           <Route path="/books/:id" element={<h1>Dettaglio Pagina</h1>}></Route>
         </Route>
       </Routes>
---
+++ webapp-react/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <h1 className="text-primary">Bool Books </h1>
+
+
+
+
+
+
+
+        export default HomePage;
