301f7741b69e1c42774c4298bf9b5b2c9271b7a4
MattiaToscano <mattia1997ct@gmail.com>
2025-06-03 10:49:03 +0200

    [UPD] Fixati problemi

--- webapp-react/src/pages/HomePage.jsx
+++ webapp-react/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { use, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import FilmsCard from '../components/FilmsCard'
 
 
 
